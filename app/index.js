85a7423cae80427f48fa06bc19f0c7b52b7b49ed
Kuroi Kyu <kuroikyu+git@gmail.com>
2017-05-25 15:29:00 +0100

    Followed steps up to adding react-router

--- app/index.js
+++ app/index.js
@@ -1,50 +1,40 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
-// require('./index.css');
-const PropTypes = require('prop-types');
+// const PropTypes = require('prop-types');
+require('./index.css');
+const App = require('./components/App');
 
-// class App extends React.Component {
+ReactDOM.render(
+  <App />,
+  document.getElementById('app'),
+);
+
+// class Badge extends React.Component {
 //   render() {
 //     return (
-//       <h1>
-//         Hello Worldios!
-//       </h1>
-//     );
+//       <div>
+//         <img 
+//           src={this.props.img} 
+//           alt='Avatar'
+//           style={{width: 100, height: 100}}
+//         />
+//         <h1>Name: {this.props.name}</h1>
+//         <h3>username: {this.props.username}</h3>
+//       </div>
+//     )
 //   }
 // }
 
+// Badge.propTypes = {
+//   img: PropTypes.string.isRequired,
+//   name:PropTypes.string.isRequired,
+//   username: PropTypes.string.isRequired,
+// }
+
 // ReactDOM.render(
-//   <App />,
-//   document.getElementById('app'),
+//   <Badge 
+//     name='Tyler McGinnis'
+//     username='tylermcginnis'
+//     img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'/>,
+//   document.getElementById('app')
 // );
-
-class Badge extends React.Component {
-  render() {
-    return (
-      <div>
-        <img 
-          src={this.props.img} 
-          alt='Avatar'
-          style={{width: 100, height: 100}}
-        />
-        <h1>Name: {this.props.name}</h1>
-        <h3>username: {this.props.username}</h3>
-      </div>
-    )
-  }
-}
-
-Badge.propTypes = {
-  img: PropTypes.string.isRequired,
-  name:PropTypes.string.isRequired,
-  username: PropTypes.string.isRequired,
-}
-
-ReactDOM.render(
-  <Badge 
-    name='Tyler McGinnis'
-    username='tylermcginnis'
-    img='https://avatars0.githubusercontent.com/u/2933430?v=3&s=460'/>,
-  document.getElementById('app')
-);
-
